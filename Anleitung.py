ebc900de1d69c8329682f554ef8076cde59c1323
AndreG-16 <adg.grau@gmail.com>
2025-09-19 15:09:54 +0200

    Anleitung hinzugefügt

---
+++ Anleitung.py
@@ -0,0 +1,14 @@
+#Wie man eine neue Datei auf GitHub hochlädt:
+
+#1. Datei auf Visual Studios erstellen
+#2. Im Terminal 'git init' um git zu starten
+#3. Im Terminal den Speicherort der Datei reinkopieren
+#4. Im Terminal 'git add'
+#5. Im Terminal 'git commit -m "Hier die Änderung Bennen"
+#6. Im Terminal: 'git config --global user.email "you@example.com"' 
+#7. Im Terminal: 'git config --global user.name "Your Name"'
+
+#Wie man Änderungen von Dateien auf GitHub hochlädt
+#1. Im Terminal 'git add .'
+#2. Unter Source Control eine Message reinschreiben und auf 'Commit' klicken
+#3. Da wo eben noch 'Commit' stand kann man jetzt auf 'Syncronize' gehen
